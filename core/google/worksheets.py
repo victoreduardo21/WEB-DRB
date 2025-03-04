9aa96608532cd8d795ea66ba55c7378b86572242
Enzo Pita <enzo@enzopita.com>
2025-03-04 18:03:56 -0300

    fix: use absolute dir instead of django's relative one

--- core/google/worksheets.py
+++ core/google/worksheets.py
@@ -1,11 +1,14 @@
 import gspread
+import os
 
 from core.google.spreadsheets.entregas import PlanilhaEntregas
 from core.google.spreadsheets.motoristas import PlanilhaMotoristas
 from core.google.spreadsheets.terminais import PlanilhaTerminais
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+credentials_path = os.path.join(current_dir, "credentials.json")
 
-gs = gspread.service_account(filename="credentials.json")
+gs = gspread.service_account(filename=credentials_path)
 sh = gs.open_by_key(key="1B6o3EpkTbpmyM4vih12WLrqnFTD_5csQRQctUCbmLRs")
 
 
